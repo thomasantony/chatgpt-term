a778f073ac98b23ae0c6874201d869c3a59ea9e2
Thomas Antony <mail@thomasantony.com>
2023-03-04 13:57:09 -0800

    Initial prototype for terminal UI

---
+++ src/app.rs
@@ -0,0 +1,283 @@
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, is_raw_mode_enabled, EnterAlternateScreen,
+    LeaveAlternateScreen,
+};
+use std::borrow::Cow;
+use std::fmt::Display;
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::layout::{Alignment, Constraint, Direction, Layout};
+use tui::style::{Color, Modifier, Style};
+use tui::text::{Span, Spans};
+use tui::widgets::{Block, Borders, Paragraph};
+
+use tui::Terminal;
+use tui_textarea::{CursorMove, Input, Key, TextArea};
+
+use crate::api::{ChatGPTClient, ChatGPTSession, ChatLogEntry};
+
+#[derive(Debug, Clone)]
+pub enum UiEvent {
+    Quit,
+    SendMessage(String),
+    SaveSession,
+    // Help(String),
+}
+
+struct ChatEntryBox<'a> {
+    textarea: TextArea<'a>,
+}
+
+impl<'a> Default for ChatEntryBox<'a> {
+    fn default() -> Self {
+        let mut textarea = TextArea::default();
+        textarea.set_block(Block::default().borders(Borders::ALL).title("Input"));
+        Self { textarea }
+    }
+}
+
+impl<'a> ChatEntryBox<'a> {
+    fn clear(&mut self) {
+        // Remove input for next input. Do not recreate `self.textarea` instance to keep undo history so that users can
+        // restore previous input easily.
+        self.textarea.move_cursor(CursorMove::End);
+        self.textarea.delete_line_by_head();
+    }
+
+    fn height(&self) -> u16 {
+        3
+    }
+
+    fn input(&mut self, input: Input) -> Option<String> {
+        match input {
+            Input {
+                key: Key::Enter, ..
+            } => {
+                let message = self.textarea.lines()[0].trim().to_string();
+                self.clear();
+                Some(message)
+            }
+            Input {
+                key: Key::Char('m'),
+                ctrl: true,
+                ..
+            } => None, // Disable shortcuts which inserts a newline. See `single_line` example
+            input => {
+                self.textarea.input(input);
+                None
+            }
+        }
+    }
+
+    fn set_error(&mut self, err: Option<impl Display>) {
+        let b = if let Some(err) = err {
+            Block::default()
+                .borders(Borders::ALL)
+                .title(format!("Input: {}", err))
+                .style(Style::default().fg(Color::Red))
+        } else {
+            Block::default().borders(Borders::ALL).title("Input")
+        };
+        self.textarea.set_block(b);
+    }
+}
+
+struct ChatTermApp<'a> {
+    current: usize,
+    session: ChatGPTSession,
+    message_area: TextArea<'a>,
+    term: Terminal<CrosstermBackend<io::Stdout>>,
+    error_message: Option<Cow<'static, str>>,
+    input: ChatEntryBox<'a>,
+}
+
+impl<'a> ChatTermApp<'a> {
+    fn new(session: ChatGPTSession) -> io::Result<Self> {
+        let mut stdout = io::stdout();
+        if !is_raw_mode_enabled()? {
+            enable_raw_mode()?;
+            crossterm::execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+        }
+        let backend = CrosstermBackend::new(stdout);
+        let term = Terminal::new(backend)?;
+
+        let message_area = ChatTermApp::create_message_area_from_session(session.get_chatlog());
+        Ok(Self {
+            current: 0,
+            session,
+            term,
+            error_message: None,
+            message_area,
+            // TODO: Add help box above input that pops up when typing /help
+            input: ChatEntryBox::default(),
+        })
+    }
+
+    // Add a new entry to the message area
+    fn add_chatlog_entry(message_area: &mut TextArea, entry: &ChatLogEntry) {
+        // Add both message and response to message_area
+        // using the insert_str method in the format "You: message"
+        message_area.insert_str(&format!("You: {}", entry.message));
+        message_area.insert_newline();
+        message_area.insert_str(&format!("Bot: {}", entry.response));
+        message_area.insert_newline();
+    }
+
+    // Clear the message area and add all the entries in the chatlog
+    fn create_message_area_from_session(chatlog: &[ChatLogEntry]) -> TextArea<'a> {
+        let mut message_area = TextArea::default();
+        message_area.set_block(Block::default().borders(Borders::ALL).title("Chat Log"));
+        message_area.set_style(Style::default().fg(Color::White));
+        message_area.set_alignment(Alignment::Left);
+        message_area.set_cursor_style(Style::default().fg(Color::Black));
+
+        for entry in chatlog.iter() {
+            message_area.insert_str(&format!("You: {}", entry.message));
+            message_area.insert_newline();
+            message_area.insert_str(&format!("Bot: {}", entry.response));
+            message_area.insert_newline();
+        }
+        message_area
+    }
+
+    fn update_ui(&mut self) -> Option<UiEvent> {
+        let input_height = self.input.height();
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(
+                [
+                    Constraint::Min(1),
+                    Constraint::Length(input_height),
+                    Constraint::Length(1),
+                    Constraint::Length(1),
+                ]
+                .as_ref(),
+            );
+
+        self.term
+            .draw(|f| {
+                let chunks = layout.split(f.size());
+
+                f.render_widget(self.message_area.widget(), chunks[0]);
+
+                // Render status line
+                let slot = format!("[{}/{}]", self.current + 1, 10);
+                let status_chunks = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints(
+                        [
+                            Constraint::Length(slot.len() as u16),
+                            Constraint::Min(1),
+                            Constraint::Length(10u16),
+                        ]
+                        .as_ref(),
+                    )
+                    .split(chunks[2]);
+                let status_style = Style::default().add_modifier(Modifier::REVERSED);
+                f.render_widget(Paragraph::new(slot).style(status_style), status_chunks[0]);
+                f.render_widget(Paragraph::new("").style(status_style), status_chunks[1]);
+                f.render_widget(Paragraph::new("0").style(status_style), status_chunks[2]);
+
+                f.render_widget(self.input.textarea.widget(), chunks[1]);
+
+                // Render message at bottom
+                let message = if let Some(message) = self.error_message.take() {
+                    Spans::from(Span::raw(message))
+                } else {
+                    Spans::from(vec![
+                        Span::raw("Press "),
+                        Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+                        Span::raw(" to quit, "),
+                        Span::styled("^S", Style::default().add_modifier(Modifier::BOLD)),
+                        Span::raw(" to save session "),
+                    ])
+                };
+                f.render_widget(Paragraph::new(message), chunks[3]);
+            })
+            .ok();
+        match crossterm::event::read().ok().map(Into::into) {
+            Some(Input { key: Key::Esc, .. }) => Some(UiEvent::Quit),
+            Some(Input {
+                key: Key::Char('s'),
+                ctrl: true,
+                alt: false,
+            }) => Some(UiEvent::SaveSession),
+            // Pass through mousescroll events to the message area
+            Some(Input {
+                key: Key::MouseScrollDown,
+                ..
+            }) => {
+                self.message_area.input(Input {
+                    key: Key::MouseScrollDown,
+                    ..Default::default()
+                });
+                None
+            }
+            Some(Input {
+                key: Key::MouseScrollUp,
+                ..
+            }) => {
+                self.message_area.input(Input {
+                    key: Key::MouseScrollUp,
+                    ..Default::default()
+                });
+                None
+            }
+            Some(input) => self.input.input(input).and_then(|message_str| {
+                if !message_str.is_empty() {
+                    Some(UiEvent::SendMessage(message_str))
+                } else {
+                    None
+                }
+            }),
+            _ => None,
+        }
+    }
+}
+
+impl<'a> Drop for ChatTermApp<'a> {
+    fn drop(&mut self) {
+        self.term.show_cursor().unwrap();
+        if !is_raw_mode_enabled().unwrap() {
+            return;
+        }
+        disable_raw_mode().unwrap();
+        crossterm::execute!(
+            self.term.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        )
+        .unwrap();
+    }
+}
+
+pub fn run(client: ChatGPTClient) -> Result<(), Box<dyn std::error::Error>> {
+    // Load chat log from chatlog.json file and deserialize it
+    // Create a new session
+    let session = client.new_session(2000).with_log_file("chatlog.json")?;
+
+    let mut app = ChatTermApp::new(session)?;
+    loop {
+        if let Some(ui_event) = app.update_ui() {
+            match ui_event {
+                UiEvent::SendMessage(message_str) => match app.session.send_message(&message_str) {
+                    Ok(entry) => ChatTermApp::add_chatlog_entry(&mut app.message_area, &entry),
+                    Err(err) => {
+                        app.input.set_error(Some(format!("Error: {:?}", err)));
+                    }
+                },
+                UiEvent::SaveSession => {
+                    if let Err(err) = app.session.save_chatlog("chatlog.json") {
+                        app.error_message = Some(format!("Error: {:?}", err).into());
+                    } else {
+                        app.error_message = Some("Saved session to chatlog.json".into());
+                    }
+                }
+                UiEvent::Quit => break,
+            }
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 // Import the library from lib.rs
-fn main() -> Result<(), serde_json::Error> {
-    // Load chat log from chatlog.json file and deserialize it
-    let chat_log: Vec<chatgpt_term::api::Message> =
-        serde_json::from_str(&std::fs::read_to_string("chatlog.json").unwrap())?;
+use chatgpt_term::api::{ChatGPTClient, ChatLogEntry};
+use std::env;
 
-    println!("{:#?}", chat_log);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get the OpenAI API key from the environment
+    let key = "OPENAI_API_KEY";
+    let openai_api_key = env::var(key).unwrap_or_else(|_| panic!("{} not set", key));
+    // Create a new client
+    let client = ChatGPTClient::new(&openai_api_key, "gpt-3.5-turbo");
+    chatgpt_term::app::run(client)?;
+
+    Ok(())
+}
+
+// Mark as unused to avoid warnings
+#[allow(dead_code)]
+fn generate_dummy_data() -> std::io::Result<()> {
+    // Generate some dummy ChatLogEntry objects
+    let mut chat_log: Vec<ChatLogEntry> = Vec::new();
+    for i in 0..10 {
+        let entry = ChatLogEntry::new(&format!("Message {}", i), &format!("Response {}", i));
+        chat_log.push(entry);
+    }
+
+    // Write the chat log to a file in prettified json format
+    let chat_log_json = serde_json::to_string_pretty(&chat_log)?;
+    std::fs::write("chatlog.json", chat_log_json)?;
+
     Ok(())
 }
